2a29dd5eea048a717ed4844ed36872bbe5e235a0
rickparrish <github@rick.parrish>
2015-12-30 22:52:16 -0500

    Add VM Rescue and VNC

---
+++ lndapi/VM/VMRescue.cs
@@ -0,0 +1,49 @@
+﻿using lndapi.Base;
+using lndapi.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lndapi
+{
+    public partial class LNDynamic
+    {
+        public async Task VMRescueAsync(int vmId)
+        {
+            await RequestAsync<VMRescueResponseModel>("vm", "rescue", new VMRescueRequestModel(_BRM, vmId));
+        }
+    }
+}
+
+namespace lndapi.VM
+{
+    public class VMRescueRequestModel : BaseRequestModel
+    {
+        public int vm_id { get; set; }
+
+        public VMRescueRequestModel(BaseRequestModel brm, int vmId)
+        {
+            this.api_id = brm.api_id;
+            this.api_key = brm.api_key;
+            this.vm_id = vmId;
+        }
+
+        public override string ToString()
+        {
+            return $"vm_id={vm_id}&{base.ToString()}";
+        }
+    }
+
+    public class VMRescueResponseModel : BaseResponseModel
+    {
+        /*
+        {"success":"no","error":"invalid vm"}
+        or
+        {"success":"no","error":"ERROR (CommandError): Unable to Rescue the specified server(s)."}
+        or
+        {"success":"yes"}
+        */
+    }
+}
---
+++ lndapi/VM/VMVNC.cs
@@ -0,0 +1,49 @@
+﻿using lndapi.Base;
+using lndapi.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lndapi
+{
+    public partial class LNDynamic
+    {
+        public async Task<string> VMVNCAsync(int vmId)
+        {
+            var Result = await RequestAsync<VMVNCResponseModel>("vm", "vnc", new VMVNCRequestModel(_BRM, vmId));
+            return Result.vnc_url;
+        }
+    }
+}
+
+namespace lndapi.VM
+{
+    public class VMVNCRequestModel : BaseRequestModel
+    {
+        public int vm_id { get; set; }
+
+        public VMVNCRequestModel(BaseRequestModel brm, int vmId)
+        {
+            this.api_id = brm.api_id;
+            this.api_key = brm.api_key;
+            this.vm_id = vmId;
+        }
+
+        public override string ToString()
+        {
+            return $"vm_id={vm_id}&{base.ToString()}";
+        }
+    }
+
+    public class VMVNCResponseModel : BaseResponseModel
+    {
+        /*
+        {"success":"no","error":"invalid vm"}
+        or
+        {"vnc_url":"https:\/\/toronto-ctrl.lunanode.com:6080\/vnc_auto.html?token=<GUID>","success":"yes"}
+        */
+        public string vnc_url { get; set; }
+    }
+}
