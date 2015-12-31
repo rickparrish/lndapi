336c709224000d4b28416af7e1a4e0430a22b4f8
rickparrish <github@rick.parrish>
2015-12-30 22:44:37 -0500

    Add VM Delete and Resize

---
+++ lndapi/VM/VMDelete.cs
@@ -0,0 +1,47 @@
+﻿using lndapi.Base;
+using lndapi.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lndapi
+{
+    public partial class LNDynamic
+    {
+        public async Task VMDeleteAsync(int vmId)
+        {
+            await RequestAsync<VMDeleteResponseModel>("vm", "delete", new VMDeleteRequestModel(_BRM, vmId));
+        }
+    }
+}
+
+namespace lndapi.VM
+{
+    public class VMDeleteRequestModel : BaseRequestModel
+    {
+        public int vm_id { get; set; }
+
+        public VMDeleteRequestModel(BaseRequestModel brm, int vmId)
+        {
+            this.api_id = brm.api_id;
+            this.api_key = brm.api_key;
+            this.vm_id = vmId;
+        }
+
+        public override string ToString()
+        {
+            return $"vm_id={vm_id}&{base.ToString()}";
+        }
+    }
+
+    public class VMDeleteResponseModel : BaseResponseModel
+    {
+        /*
+        {"success":"no","error":"invalid vm"}
+        or
+        {"success":"yes"}
+        */
+    }
+}
---
+++ lndapi/VM/VMResize.cs
@@ -0,0 +1,51 @@
+﻿using lndapi.Base;
+using lndapi.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lndapi
+{
+    public partial class LNDynamic
+    {
+        public async Task VMResizeAsync(int vmId, int planId)
+        {
+            await RequestAsync<VMResizeResponseModel>("vm", "resize", new VMResizeRequestModel(_BRM, vmId, planId));
+        }
+    }
+}
+
+namespace lndapi.VM
+{
+    public class VMResizeRequestModel : BaseRequestModel
+    {
+        public int vm_id { get; set; }
+        public int plan_id { get; set; }
+
+        public VMResizeRequestModel(BaseRequestModel brm, int vmId, int planId)
+        {
+            this.api_id = brm.api_id;
+            this.api_key = brm.api_key;
+            this.vm_id = vmId;
+            this.plan_id = planId;
+        }
+
+        public override string ToString()
+        {
+            return $"vm_id={vm_id}&plan_id={plan_id}&{base.ToString()}";
+        }
+    }
+
+    public class VMResizeResponseModel : BaseResponseModel
+    {
+        /*
+        {"success":"no","error":"invalid vm"}
+        or
+        {"success":"no","error":"invalid plan"}
+        or
+        {"success":"yes"}
+        */
+    }
+}
