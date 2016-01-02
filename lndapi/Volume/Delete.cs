e32d11bd349d3293b6ddaa50de1e373d2a460ee3
rickparrish <github@rick.parrish>
2016-01-01 20:32:49 -0500

    Add Volume Delete

---
+++ lndapi/Volume/Delete.cs
@@ -0,0 +1,23 @@
+﻿using lndapi.Base;
+using lndapi.Volume;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lndapi
+{
+    public partial class LNDynamic
+    {
+        /// <summary>
+        /// Delete a volume
+        /// </summary>
+        /// <param name="region">The name of the region to delete the volume from</param>
+        /// <param name="volumeId">The id of the volume to delete</param>
+        public async Task VolumeDeleteAsync(string region, int volumeId)
+        {
+            await RequestAsync<BaseResponseModel>("volume", "delete", new VolumeBaseRequestModel(region, volumeId));
+        }
+    }
+}
